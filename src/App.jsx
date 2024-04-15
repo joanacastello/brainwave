014f5c6007b8686e9b23e55cfb64331c20a7e72e
JOANA CASTELLÓ VISIEDO <castellovisiedo@gmail.com>
2024-04-15 18:07:22 +0200

    Implement Footer, Pricing and Roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +17,10 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
